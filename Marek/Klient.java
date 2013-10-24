d576a45aa962916dd34f1ba0968d94a56f32afd1
DjVidiX <djvidix.ms@gmail.com>
2013-10-24 12:59:04 +0200

    Add Player class

--- Marek/Klient.java
+++ Marek/Klient.java
@@ -6,32 +6,6 @@ import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 
 public class Klient {
-	
-	private String name;
-	private Interfejs interfejs;
-	private String nazwa;
-	private int id;
-	
-	public Klient() {
-		this.name = "Undefined";
-	}
-	
-	public Klient(String name) {
-		this.name = name;
-	}
-	
-	public void registry(String nazwa) {
-		try {
-            interfejs = (Interfejs) Naming.lookup(nazwa);
-            id = interfejs.registryClient(name);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
-	}
 
     public static void main(String[] args) {
 
@@ -46,8 +20,19 @@ public class Klient {
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
         }
-
-        Klient k = new Klient(name);
-        k.registry(nazwa);
+	
+	try {
+            interfejs = (Interfejs) Naming.lookup(nazwa);
+            id = interfejs.registryClient(name);
+            lista_graczy = interfejs.getClientList();
+            
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+	}
     }
 }
---
+++ Marek/Player.java
@@ -0,0 +1,16 @@
+class Player {
+	private String name;
+	//private Interfejs interfejs;
+	private String nazwa;
+	private int id;
+	
+	public Player() {
+		this.name = "Undefined";
+	}
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	
+}
